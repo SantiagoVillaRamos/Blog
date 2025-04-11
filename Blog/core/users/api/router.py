31603679ad7030b7ba6cd300f0a45b37e3e119fd
santiago <ramossoloyo12345@gmail.com>
2025-04-11 16:31:17 -0500

    se crea la clase con su logica para traer los datos del usuario

--- Blog/core/users/api/router.py
+++ Blog/core/users/api/router.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, UserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('auth/user', UserView.as_view(), name='user'),
+    # JWT authentication endpoints
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- Blog/core/users/api/views.py
+++ Blog/core/users/api/views.py
@@ -2,7 +2,9 @@ from users.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 # clase encargada de registrar un nuevo usuario
 class RegisterView(APIView):
@@ -13,3 +15,11 @@ class RegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors ,status=status.HTTP_400_BAD_REQUEST)
+    
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    # devolver los datos del usuario
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
