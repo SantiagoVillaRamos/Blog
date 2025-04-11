d756cc327a4c0c4e4e393a5a35ad206472724b06
santiago <ramossoloyo12345@gmail.com>
2025-04-11 10:11:00 -0500

    Eliminacion de todo el proyecto

--- Blog/core/core/comments/api/router.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from comments.api.views import CommentViewSet
-
-router_comment = DefaultRouter()
-router_comment.register(prefix='comments', basename='comments', viewset=CommentViewSet)
