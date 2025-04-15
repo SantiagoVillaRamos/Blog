117abef22599bde9c48cd42d17f4613eab88950f
santiago <ramossoloyo12345@gmail.com>
2025-04-14 19:41:03 -0500

    subor archivos...

--- Blog/core/categories/api/views.py
+++ Blog/core/categories/api/views.py
@@ -1,11 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Categorie
+from django_filters.rest_framework import DjangoFilterBackend
 from . serializer import CategorieSerializer
 from .permisions import IsAdminOrReadOnly
 
 class CategoryApiViewSet(ModelViewSet):
     
-    queryset = Categorie.objects.all()
     serializer_class = CategorieSerializer
     permission_classes = [IsAdminOrReadOnly]
-    
+    queryset = Categorie.objects.filter(publised=True)
+    lookup_field = 'slug'
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title']
